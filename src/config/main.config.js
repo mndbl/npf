d3006e3560410ac0af995a801885cc4fd515417e
José Mendible <66981978+mndbl@users.noreply.github.com>
2025-01-05 10:16:32 -0700

    Update main.config.js

--- src/config/main.config.js
+++ src/config/main.config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://wimmoney-back.42web.io'
+export const API_URL = 'http://new-project-back.test/api'
 export const registers_URL = `${API_URL}/data/registers`
 export const accounts_URL = `${API_URL}/data/settings/accounts`
 export const accounts_categories_URL = `${API_URL}/data/settings/account-categories`
